c4d264e69f9ea300e0564fd1879bd8ceae2c6e06
Dilantha_MacbookAir <dilantha_macbookair@MacBookAir.localdomain>
2025-12-29 01:39:30 -0600

    Update FeaturesShowcase component

--- frontend/src/Components/FeaturesShowcase.jsx
+++ frontend/src/Components/FeaturesShowcase.jsx
@@ -40,7 +40,7 @@ const FeaturesShowcase = () => {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentSlide((prev) => (prev + 1) % slides.length);
-        }, 5000);
+        }, 8000);
 
         return () => clearInterval(interval);
     }, [slides.length]);
